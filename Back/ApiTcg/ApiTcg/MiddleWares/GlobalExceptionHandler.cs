44294f3abba8e76f257006d7a6f85e66714a13e1
Renon bart <b.renon@mail.com>
2026-05-19 13:15:11 +0200

    modiff du proj de base premère parti

---
+++ Back/ApiTcg/ApiTcg/MiddleWares/GlobalExceptionHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTcg.MiddleWares;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+
+        if (httpContext.Request.Path.StartsWithSegments("/swagger"))
+        {
+            return false;
+        }
+
+        var problemDetails = new ProblemDetails();
+
+        switch (exception)
+        {
+            case ArgumentException:
+                problemDetails.Status = StatusCodes.Status400BadRequest;
+                problemDetails.Title = exception.Message;
+                break;
+
+            case KeyNotFoundException:
+                problemDetails.Status = StatusCodes.Status404NotFound;
+                problemDetails.Title = exception.Message;
+                break;
+
+            default:
+                problemDetails.Status = StatusCodes.Status500InternalServerError;
+                problemDetails.Title = "Une erreur interne est survenue";
+                break;
+        }
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+        httpContext.Response.ContentType = "application/problem+json";
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
