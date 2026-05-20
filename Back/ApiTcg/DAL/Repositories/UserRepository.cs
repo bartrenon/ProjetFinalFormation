97bd69745d92084791a6eed0b8ef019603e27661
Renon bart <b.renon@mail.com>
2026-05-20 14:18:41 +0200

    base repo user

---
+++ Back/ApiTcg/DAL/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using DAL.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly string _connectionString;
+
+    public UserRepository(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+    }
+}
