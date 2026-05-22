42b97be4e7107a7fde5f13e84e6546fcc500f5c9
Renon bart <b.renon@mail.com>
2026-05-22 12:06:30 +0200

    mise en place du service user coté back

---
+++ Front/poke-tcg/src/app/models/User/UserCreate.ts
@@ -0,0 +1,6 @@
+export interface UserCreate
+{
+    Username: string;
+    Email: string;
+    PasswordHash: string;
+}
--- Front/poke-tcg/src/app/services/user.ts
+++ Front/poke-tcg/src/app/services/user.ts
@@ -1,6 +1,22 @@
+import { UserCreate } from '../models/User/UserCreate';
+
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class User {}
+export class User {
+  private readonly _url : string = '';
+
+  constructor(private _http: HttpClient) {}
+
+  createUser(user: UserCreate) : Observable<UserCreate>
+  {
+    return this._http.post<UserCreate>(this._url, user);
+  }
+
+}
