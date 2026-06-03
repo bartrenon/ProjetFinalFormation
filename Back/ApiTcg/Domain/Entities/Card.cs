9a057b53851ff11c6ea8c365425b278c9c28fa6c
Renon bart <b.renon@mail.com>
2026-06-03 15:20:41 +0200

    modiff tables card + model card

---
+++ Back/ApiTcg/Domain/Entities/Card.cs
@@ -0,0 +1,18 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Domain.Entities;
+
+public class Card
+{
+    
+   public string Id { get; set; } = "";
+
+   public string SetId { get; set; } = "";
+
+   public string Name { get; set; } = "";
+
+   public string LocalId { get; set; } = "";
+
+   public string? Image { get; set; }
+
+}
