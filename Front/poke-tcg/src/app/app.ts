28bfd2d23b30b0cd8fcb1371fdce993dcac010ce
Renon bart <b.renon@mail.com>
2026-07-14 12:07:24 +0200

    cacher la nav bar si le user n'est pas co

--- Front/poke-tcg/src/app/app.ts
+++ Front/poke-tcg/src/app/app.ts
@@ -1,13 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/layouts/navbar/navbar";
+import { UserService } from './services/user/user-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('poke-tcg');
+  userService = inject(UserService);
 }
