01ba2587cdfbee75565ef0cf26225e84e467407c
Renon bart <b.renon@mail.com>
2026-07-16 16:07:14 +0200

    mise à jour de la nav bar + changement du disgn

--- Front/poke-tcg/src/app/components/layouts/navbar/navbar.ts
+++ Front/poke-tcg/src/app/components/layouts/navbar/navbar.ts
@@ -1,25 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { UserService } from '../../../services/user/user-service';
+
+interface Generation {
+  label: string;
+  slug: string;
+}
 
 @Component({
   selector: 'app-navbar',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  public searchTerm = signal('');
+  public isMenuOpen = signal(false);
 
-  public searchTerm = '';
   private _userService = inject(UserService);
-
-  constructor(private router: Router) {}
+  private _router = inject(Router);
 
   onSearch() {
+    
   }
 
   onLogout() {
     this._userService.logout();
+    this._router.navigate(['/login']);
   }
 }
