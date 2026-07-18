2fd8b10dbbb3775eb67dfeed24567b81371663ff
Renon bart <b.renon@mail.com>
2026-07-18 18:12:14 +0200

    moddif visuel mineur

--- Front/poke-tcg/src/app/services/card-services.ts
+++ Front/poke-tcg/src/app/services/card-services.ts
@@ -11,7 +11,7 @@ export class CardServices {
   private readonly _url = 'https://localhost:7009/apiTcg/Card';
 
   private readonly offset = 5;
-  private readonly pageSize = 20;
+  private readonly pageSize = 24;
 
   constructor(private _http: HttpClient) {}
 
