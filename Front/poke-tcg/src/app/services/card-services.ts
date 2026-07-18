01d87a47dd407fb2fb013698ff3636fef61741bf
Renon bart <b.renon@mail.com>
2026-07-18 09:19:47 +0200

    mise en place de nouvelles route pour les news composant

--- Front/poke-tcg/src/app/services/card-services.ts
+++ Front/poke-tcg/src/app/services/card-services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Card } from '../models/card/card';
 
 @Injectable({
@@ -10,6 +10,9 @@ export class CardServices {
 
   private readonly _url = 'https://localhost:7009/apiTcg/Card';
 
+  private readonly offset = 5;
+  private readonly pageSize = 20;
+
   constructor(private _http: HttpClient) {}
 
   getCardById(id: string): Observable<Card>
@@ -17,4 +20,17 @@ export class CardServices {
       return this._http.get<Card>
       (`${this._url}/${encodeURIComponent(id)}`);
   }
+
+  getCards(name?: string): Observable<Card[]> {
+    let params = new HttpParams()
+      .set('pageNumber', this.offset)   
+      .set('pageSize', this.pageSize);
+
+    if (name) {
+      params = params.set('name', name);
+    }
+
+    return this._http.get<Card[]>(this._url, { params });
+  }
+
 }
