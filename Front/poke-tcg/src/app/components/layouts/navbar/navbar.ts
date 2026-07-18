567f6421d5fb1218dd8dda833d91d695479f44a1
Renon bart <b.renon@mail.com>
2026-07-18 18:12:52 +0200

    modiff pour changer le texte dans la bar de recherche ou la rendre non utilisable en fonction de la page ou l'on se trouve

--- Front/poke-tcg/src/app/components/layouts/navbar/navbar.ts
+++ Front/poke-tcg/src/app/components/layouts/navbar/navbar.ts
@@ -1,6 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
+import { filter, map, startWith } from 'rxjs';
 import { UserService } from '../../../services/user/user-service';
 
 interface Generation {
@@ -20,6 +22,30 @@ export class Navbar {
 
   private _userService = inject(UserService);
   private _router = inject(Router);
+
+  private currentUrl = toSignal(
+    this._router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((event) => (event as NavigationEnd).urlAfterRedirects),
+      startWith(this._router.url)
+    ),
+    { initialValue: this._router.url }
+  );
+
+  public searchPlaceholder = computed(() => {
+    const url = this.currentUrl();
+
+    if (url.startsWith('/sets')) return 'Rechercher un set';
+    if (url.startsWith('/set/') || url.startsWith('/cards')) return 'Rechercher une carte';
+
+    return 'Rechercher';
+  });
+
+  public isSearchDisabled = computed(() => {
+  const url = this.currentUrl();
+
+  return url.startsWith('/profil') || url.startsWith('/card/');
+});
 
   onSearch() {
     
