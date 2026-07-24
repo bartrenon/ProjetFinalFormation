c35c6ddc0fe91aca84fb0f941dec25b123450f19
Renon bart <b.renon@mail.com>
2026-07-24 10:29:56 +0200

    page profil user template de base

--- Front/poke-tcg/src/app/components/user/profil/profil.ts
+++ Front/poke-tcg/src/app/components/user/profil/profil.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profil',
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './profil.html',
   styleUrl: './profil.scss',
 })
-export class Profil {}
+export class Profil implements OnInit{
+
+  email = '';
+
+ ngOnInit(): void {
+
+  }
+}
